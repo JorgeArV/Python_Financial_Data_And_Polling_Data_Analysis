6cea6f9dee3a14fd9e21778440e976f0ac26ac84
JorgeArV <jorge.arriola.villafuerte@gmail.com>
2020-10-17 14:19:05 -0400

    updated PyBankScript

--- PyBank/pybank_main.py
+++ PyBank/pybank_main.py
@@ -3,13 +3,34 @@ import csv
 
 # Path to collect data from the PayPoll_Resources folder
 filepath = os.path.join("PyBank", "PyBank_Resources", "budget_data.csv")
+
+#
+Months = []
+ProfitsorLosses = []
+Changes_PandL = []
 # Read in the CSV file
 with open(filepath, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
+    header = next(csvreader)
 
     # Loop through the data
     for row in csvreader:
-        
-        print(row)
+        Months.append(row[0])
+        ProfitsorLosses.append(int(row[1]))
 
-        break
+    print(Months[1+1])
+
+    for i in range(len(Months)):
+        if ProfitsorLosses[i] == ProfitsorLosses [0]:
+            ""
+        else:    
+            Datapoint = ProfitsorLosses [i] - ProfitsorLosses [i-1]
+            Changes_PandL.append(Datapoint)
+
+    TotalMonths = len(Months)
+    Total = sum(ProfitsorLosses)
+    AverageChange = sum(Changes_PandL) / len(Changes_PandL)    
+    print(TotalMonths)
+    print(Total)
+    print(AverageChange)   
+    
