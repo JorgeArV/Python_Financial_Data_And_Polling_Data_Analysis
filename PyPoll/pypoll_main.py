bed60f3cda1050c30ab0df244968e9c62689e5fb
JorgeArV <jorge.arriola.villafuerte@gmail.com>
2020-10-17 16:33:57 -0400

    PyPoll Answer

--- PyPoll/pypoll_main.py
+++ PyPoll/pypoll_main.py
@@ -13,6 +13,7 @@ CorreyCounter = 0
 LiCounter = 0
 OTooleyCounter = 0
 
+
 # Read in the CSV file
 with open(filepath, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
@@ -37,12 +38,41 @@ with open(filepath, 'r') as csvfile:
             LiCounter = LiCounter + 1
         elif Votes[z] == ListofC[3]:
             OTooleyCounter = OTooleyCounter + 1    
-    print(KhanCounter) 
-    print(CorreyCounter)
-    print(LiCounter)
-    print(OTooleyCounter)
 
-        
+    #Create
+    VotesperC = [KhanCounter, CorreyCounter, LiCounter, OTooleyCounter]
+    if max(VotesperC) == KhanCounter:
+        Winner = "Khan"
+    elif max(VotesperC) == CorreyCounter:
+        Winner = "Correy"
+    elif max(VotesperC) == LiCounter:
+        Winner = "Li"
+    else:
+        Winner = "O'Tooley"
+
 #Total votes
     TotalVotes = len(VoterID)
-    print(TotalVotes)   
+    TotalVotesStatement = f"Total Votes: {TotalVotes}"
+#KhanResults
+    KhanResults = f"Khan: {round(KhanCounter/TotalVotes*100,3)}00% ({KhanCounter})"
+#CorreyResults
+    CorreyResults = f"Correy: {round(CorreyCounter/TotalVotes*100,3)}00% ({CorreyCounter})"
+#LiResults
+    LiResults = f"Li: {round(LiCounter/TotalVotes*100,3)}00% ({LiCounter})"
+#O'TooleyResults
+    OTooleyResults = f"O'Tooley: {round(OTooleyCounter/TotalVotes*100,3)}00% ({OTooleyCounter})"
+    #Winner
+    WinnerStatement = f"Winner: {Winner}"
+
+    PyPollAnswer = "Election Results" + "\n" + "-------------------------" + "\n" + TotalVotesStatement + "\n" + "-------------------------" + "\n" + KhanResults + "\n" + CorreyResults + "\n" + LiResults + "\n" + OTooleyResults + "\n" +"-------------------------" + "\n" + WinnerStatement + "\n" + "-------------------------"
+
+    print(PyPollAnswer)
+
+#Creating a text file:
+filename = "PyPoll/PyPollAnwer.txt"
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+with open(filename,"w") as datafile:
+    writer = csv.writer(datafile)
+    datafile.write(PyPollAnswer)
+
